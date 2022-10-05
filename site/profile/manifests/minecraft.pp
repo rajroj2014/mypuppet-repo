b7d01e624c5b9865adc46cf11f0d2bb5f0f4a690
rajroj2014 <50073548+rajroj2014@users.noreply.github.com>
2022-10-05 22:23:27 +0300

    Update minecraft.pp

--- site/profile/manifests/minecraft.pp
+++ site/profile/manifests/minecraft.pp
@@ -2,7 +2,7 @@ class profile::minecraft {
  file { '/root/minecraft':
     ensure => 'directory',
     owner  => 'root',
-    group  => 'wheel',
-    mode   => '0750',
+    group  => 'root',
+    mode   => '0777',
   }
 }
